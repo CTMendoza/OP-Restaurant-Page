73ffb3440dcf54c0c8d91cc315b97de9a27cd510
Christopher Mendoza <tiptop1112@gmail.com>
2024-11-25 10:40:56 -0800

    Started building out the menu content in menu.js imported food images into menu.js and built arrays that hold objects that represent each menu item

---
+++ src/menu.js
@@ -0,0 +1,52 @@
+import halohalo from './images/HaloHalo.webp';
+import karekare from './images/karekare.jpg';
+import munggo from './images/Munggo.jpg';
+import palabok from './images/palabok.jpg';
+import sinigang from './images/sinigang.webp';
+import sisig from './images/Sisig.jpeg';
+import turon from './images/turon.jpg';
+
+const entreeMenu = [
+    {
+        name:'Kare Kare',
+        description:'Kare-kare is a Philippine stew (kare derives from "curry") that features a thick savory peanut sauce. It is generally made from a base of stewed oxtail, beef tripe, pork hocks, calves" feet, pig"s feet or trotters, various cuts of pork, beef stew meat, and occasionally offal.',
+        image: karekare
+    },
+    {
+        name:'Munggo',
+        description:'A Filipino savory mung bean soup. It is made with mung beans, garlic, tomatoes, onions, various vegetables, and patis (fish sauce). It is cooked with pork, tinapa (smoked fish), daing (dried fish), or other seafood and meat. It is also commonly garnished with chicharon.',
+        image: munggo
+    },
+    {
+        name: 'Palabok',
+        description:'Pancit Palabok is a Filipino rice noodle dish with a rich pork and shrimp sauce, similar to a ragu. Also known as Pancit Luglug or Pancit Malabon. Garnished with smoked fish, eggs, and crumbled chicharron.',
+        image: palabok
+    },
+    {
+        name: 'Sinigang',
+        description: 'Sinigang is a Filipino soup or stew characterized by its sour and savory taste. It is most often associated with tamarind (Filipino: sampalok), although it can use other sour fruits and leaves as the souring agent such as unripe mangoes or rice vinegar.',
+        image: sinigang
+    },
+    {
+        name: 'Sisig',
+        description:'A Filipino dish made from pork jowl and ears (maskara), pork belly, and chicken liver, which is usually seasoned with calamansi, onions, and chili peppers. It originates from the Pampanga region in Luzon. Kapampangan sisig served on a hot plate.',
+        image: sisig
+    },
+]
+
+const dessertMenu = [
+    {
+        name: 'Halo Halo',
+        description: 'A popular dessert in the Philippines that"s a combination of shaved ice layered with a variety of different ingredients: tropical fruits, ice cream, gelatin-based candies made from coconut water, and so much more.',
+        image: halohalo
+    },
+    {
+        name: 'Turon',
+        description: 'A popular Filipino snack or dessert made of bananas rolled in a spring roll wrapper, fried, and coated with caramelized brown sugar',
+        image: turon
+    }
+]
+
+function menu() {
+    
+}
